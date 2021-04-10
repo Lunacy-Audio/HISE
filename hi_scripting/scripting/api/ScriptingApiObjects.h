eb1fa41b41ca9fc4ede5e32a56ec660f87fa363d
Casey <admin@lunacy.audio>
2021-04-10 10:59:16 -0700

    Add new function to File API moveFileTo

--- hi_scripting/scripting/api/ScriptingApiObjects.h
+++ hi_scripting/scripting/api/ScriptingApiObjects.h
@@ -364,6 +364,9 @@ namespace ScriptingObjects
 		/** Checks if this file exists and is a directory. */
 		bool isDirectory() const;
 
+		/** Moves or renames file and returns true if operation succeeded */
+		bool moveFileTo(String targetLocation);
+
 		/** Deletes the file or directory WITHOUT confirmation. */
 		bool deleteFileOrDirectory();
 
